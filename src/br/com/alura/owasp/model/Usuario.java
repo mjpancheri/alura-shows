00dce8f95c7173519dad27709861f1d4428813d3
Marcio Pancheri <mjpancheri@gmail.com>
2025-02-26 20:46:12 -0300

    add EntityManager and improve login form

--- src/br/com/alura/owasp/model/Usuario.java
+++ src/br/com/alura/owasp/model/Usuario.java
@@ -1,67 +1,81 @@
 package br.com.alura.owasp.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-
 @Entity
 public class Usuario implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Id
-	private String email;
-	private String senha;
-	private String nome;
-	private String nomeImagem;
-	@ManyToMany(fetch = FetchType.EAGER)
-	private List<Role> roles = new ArrayList<>();
-	
-	public Usuario(){}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(unique = true)
+    private String email;
+    private String senha;
+    private String nome;
+    private String nomeImagem;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<Role> roles = new ArrayList<>();
 
-	public String getEmail() {
-		return email;
-	}
+    public Usuario() {
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public String getSenha() {
-		return senha;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public List<Role> getRoles() {
-		return roles;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setRoles(List<Role> roles) {
-		this.roles = roles;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
+    public String getSenha() {
+        return senha;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public List<Role> getRoles() {
+        return roles;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
 
-	public String getNomeImagem() {
-		return nomeImagem;
-	}
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 
-	public void setNomeImagem(String nomeImagem) {
-		this.nomeImagem = nomeImagem;
-	}
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNomeImagem() {
+        return nomeImagem;
+    }
+
+    public void setNomeImagem(String nomeImagem) {
+        this.nomeImagem = nomeImagem;
+    }
 
 }
